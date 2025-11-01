3da0a64251665020187b30fc11ffdb1a3ec1ac0c
thenamelessdev <aron.minarovits@gmail.com>
2025-11-01 15:28:56 +0100

    if i evel host it on a https site then it will be easier

--- public/index.js
+++ public/index.js
@@ -1,7 +1,14 @@
 const cookieCounter = document.getElementById("clicks");
 const cookieBtn = document.getElementById("cookieBtn");
 const buy0Btn = document.getElementById("buy0");
-const wss = new WebSocket(`ws://${location.hostname}:5050`);
+let protocol;
+if (location.protocol == "http:") {
+    protocol = "ws://";
+}
+else {
+    protocol = "wss://"
+}
+const wss = new WebSocket(`${protocol}${location.hostname}:5050`);
 
 wss.addEventListener("message", (event) => {
     cookieCounter.textContent = `Clicks: ${event.data}`;
